a19550eb7c5f6aa70a4f473c86b4adfae3c9f723
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-23 21:45:43 +0900

    ルートパラメータ利用

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,25 @@ EOF;
 Route::get('hello', function () use ($html) {
     return $html;
 });
+
+Route::get('hello/{msg}',function ($msg) {
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+  <h1>Hello</h1>
+  <p>{$msg}</p>
+  <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+return $html;
+});
