4578d8f1e9ba310d3ffe3ac7baf33b9add4817c7
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-24 15:49:20 +0900

    bladeテンプレート作成、フォームの使い方

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello/Index</title>
+  <style>
+  body {
+    font-size: 16pt;
+    color: #999;
+  }
+  h1 {
+    font-size: 50pt;
+    text-align: right;
+    color: #f6f6f6;
+    margin: -20px 0 -30px 0;
+    letter-spacing: -4pt;
+  }
+  </style>
+</head>
+<body>
+  <h1>Blade/Index</h1>
+  <p>{{$msg}}</p>
+  <form method="POST" action="/hello">
+    @csrf
+    <input type="text" name="msg">
+    <input type="submit">
+  </form>
+</body>
+</html>
