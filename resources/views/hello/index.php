1ae77b9ea146bcfcf71817267e7639a1c3ea8f87
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-24 14:51:50 +0900

    hello.index viewファイル作成

---
+++ resources/views/hello/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello/Index</title>
+  <style>
+  body {
+    font-size: 16pt;
+    color: #999;
+  }
+  h1 {
+    font-size: 100pt;
+    text-align: right;
+    color: #f6f6f6;
+    margin: -50px 0 -100px 0;
+  }
+  </style>
+</head>
+<body>
+  <h1>Index</h1>
+  <p>This is a sample page with php-template.</p>
+</body>
+</html>
