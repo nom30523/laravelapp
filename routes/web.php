4557886fe5ccd08eaf5943a228b6216f25218a77
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-28 14:45:15 +0900

    session用アクション、ビュー作成

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
 Route::resource('rest', 'RestappController');
+Route::get('hello/session', 'HelloController@ses_get');
+Route::post('hello/session', 'HelloController@ses_put');
